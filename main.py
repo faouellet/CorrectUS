1841bedde77be46bac372a9662a6902c85dbd9ab
faouellet <faouellet@gmail.com>
2015-10-28 19:24:50 -0400

    Adding basic config file parsing

--- main.py
+++ main.py
@@ -2,21 +2,46 @@ import argparse
 import os
 import subprocess
 import re
+import yaml
 
 import pdb
 
-def main():
-    parser = argparse.ArgumentParser(description="CorrectUS arguments parser")
-    parser.add_argument('dir', help="Top level directory containing the assignments to grade", action='store')
+def parse_config(config):
+    error_cost = {}
+    conf = open(config)
+    data = yaml.safe_load(conf)
+    conf.close()
+    for d in data:
+        error_cost[d['error']['type']] = d['error']['cost']
 
+    return error_cost
+
+def init_parser():
+    parser = argparse.ArgumentParser(description="CorrectUS options:")
+    parser.add_argument('-dir=', dest='dir', metavar='directory', 
+                        help="""Specifies the top level directory containing the assignments to grade. 
+                                When the value is empty it will assume the current directory is the one containing the assignment""")
+    #TODO: Add argument for outputting to file or stdout
+    parser.add_argument('-config=', dest='config', metavar='config file', 
+                        help="Specifies the configuration file in YAML format. When the value is empty it will use the default configuration file provided.")
+
+    return parser
+
+def main():
+    parser = init_parser()
     args = parser.parse_args()
     directory = args.dir
+
+    marking_scheme = parse_config(args.config)
+    print marking_scheme
+
+    command = "clang-tidy -checks=-*,clang-analyzer-* {0} --"
 
     if os.path.isdir(directory):
         for f in os.listdir(directory):
             extension = os.path.splitext(f)[1][1:]
             if extension in ["cpp", "h"]:
-                p = subprocess.Popen(["clang-tidy", directory + f, "--"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                p = subprocess.Popen(command.format(directory + f).split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 output = p.communicate()[0]
                 osplit = output.split('\n')
                 for line in osplit:
